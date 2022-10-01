d1db4eae30b75d049511cd9449be5c0fc650511d
Yusuf Yazıcı <63202456+yusufYAZICI155@users.noreply.github.com>
2022-10-01 15:52:01 +0300

    1.0.3 update

--- main.py
+++ main.py
@@ -9,27 +9,28 @@ import moviepy.editor as moviepy
 
 # cleaning files
 try:
-    os.remove("output.%(ext)s")
+    os.remove("output.m4a")
 except:
     pass
 
 
 url = ""
-type("")
+
+# input url by user
 while "youtube" not in url:
     url = input("Youtube link: ")
     if "youtube" not in url:
         print("Please enter youtube link.")
 
+# options
 ydl_opts = {
     "format":"bestaudio/best",
     "outtmpl" : "output.%(ext)s",
 }
 
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
+# download
+youtube_dl.YoutubeDL(ydl_opts).download([url])
+
+# m4a to mp3 convert
 clip = moviepy.AudioFileClip("output.m4a")
 clip.write_audiofile("output.mp3")
-
-
-print("Selam")
